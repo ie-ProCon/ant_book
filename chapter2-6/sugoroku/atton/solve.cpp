16bcb16bf9e9c4b99638bc9c92d894f034eea187
e115763 <e115763@gmail.com>
2013-11-30 22:35:41 +0900

    Solve sugoroku from atton

---
+++ chapter2-6/sugoroku/atton/solve.cpp
@@ -0,0 +1,41 @@
+/* Programming Contest Challange Book P.109
+ * sugoroku
+ */
+
+#include<iostream>
+
+int extgcd(int a, int b, int& x, int&y) {
+    int d = a;
+
+    if (b != 0) {
+        d = extgcd(b, a % b, y, x);
+        y -= (a / b) * x;
+    } else {
+        x = 1; y = 0;
+    }
+
+    return d;
+}
+
+int main(int argc, char const* argv[]) {
+    int a, b;
+    std::cin >> a >> b;
+
+    int x, y;
+    extgcd(4, 11, x, y);;
+
+    if (x > 0) {
+        std::cout << x << " " << 0 << " ";
+    } else {
+        std::cout << 0 << " " << -x << " ";
+    }
+
+    if (y > 0) {
+        std::cout << y << " " << 0 << " ";
+    } else {
+        std::cout << 0 << " " << -y << " ";
+    }
+
+    std::cout << std::endl;
+    return 0;
+}
